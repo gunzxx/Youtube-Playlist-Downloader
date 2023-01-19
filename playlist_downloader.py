9dcfd711c2ffa29a44ae7f613bdbdba4dcc6eae1
gunzxx <ggm5850@gmail.com>
2023-01-19 12:24:15 +0700

    Update playlist_downloader.py

--- playlist_downloader.py
+++ playlist_downloader.py
@@ -1,8 +1,9 @@
 from pytube import Playlist
 
-# link = input("Link : ")
+link = input("Masukkan link : ")
 
-yt_playlist = Playlist("https://www.youtube.com/playlist?list=PL9At9z2rvOC-Z6Gt8uO1XMp4oyMlE3gml") #ganti link nya dengan link anda sendiri
+# yt_playlist = Playlist("https://www.youtube.com/playlist?list=PL9At9z2rvOC-Z6Gt8uO1XMp4oyMlE3gml") #ganti link nya dengan link anda sendiri
+yt_playlist = Playlist(link)
 
 for video in yt_playlist.videos:
     video.streams.get_highest_resolution().download()
