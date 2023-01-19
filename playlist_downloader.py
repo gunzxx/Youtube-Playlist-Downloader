78aa7fb80c89f24b38ae96e2fb9551629bfcc4a4
gunzxx <ggm5850@gmail.com>
2023-01-19 12:11:15 +0700

    Update playlist_downloader.py

--- playlist_downloader.py
+++ playlist_downloader.py
@@ -5,7 +5,7 @@ from pytube import Playlist
 yt_playlist = Playlist("https://www.youtube.com/playlist?list=PL9At9z2rvOC-Z6Gt8uO1XMp4oyMlE3gml") #ganti link nya dengan link anda sendiri
 
 for video in yt_playlist.videos:
-    video.streams.get_highest_resolution().download("G:\Laravel\Python\singger")
+    video.streams.get_highest_resolution().download()
     print("video : ", video.title)
 
 
